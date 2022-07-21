e261b438b855ede41dfd7ad5d8d86189cd81483e
Lorenzo Bodini <lorenzo.bodini.private@gmail.com>
2022-07-21 17:31:54 +0200

    improvements, wrapper functions ADD reversed property to Condition, it meets only if all Filters are negative ADD Forks.attach_fork, for adding an already initialized Fork object ADD Forks.attach_join_detach wrapper for adding a fork, waiting for it to complete, detaching it and returning the stop_return value of Fork.completed MOD now default timeout for wait_for is 0, meaning no timeout MOD changed behaviour of Condition.stop_return ADD automatic thread creator for wait_for function

--- daemon.py
+++ daemon.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 from telebotapi import TelegramBot
 from time import sleep
 from uuid import uuid4
+from threading import Thread
 
 
 class Filter:
@@ -25,9 +26,10 @@ class Filter:
 
 
 class Condition:
-    def __init__(self, *filters: Filter, callback=lambda l: None, stop_return=None):
+    def __init__(self, *filters: Filter, callback=lambda l: None, stop_return=None, reversed=False):
         self.callback = callback
-        self.stop_return = stop_return
+        self.stop_return_ = stop_return
+        self.reversed = reversed
         self.filters = list(filters)
 
     def add_filter(self, *f):
@@ -35,13 +37,24 @@ class Condition:
             self.filters.append(i)
 
     def meet(self, msg):
-        return all(map(lambda l: l.call(msg), self.filters))
+        ret = all(map(lambda l: l.call(msg), self.filters))
+        if self.reversed:
+            return not ret
+        return ret
 
     def __str__(self):
         return f"Condition(filters=[{', '.join(map(lambda l: str(l), self.filters))}], callback={self.callback})"
 
     def __repr__(self):
         return str(self)
+
+    def stop_return(self, msg=None):
+        if msg is None:
+            return self.stop_return_
+        if callable(self.stop_return_):
+            return self.stop_return_(msg)
+        else:
+            return self.stop_return_
 
 
 class Fork:
@@ -51,11 +64,11 @@ class Fork:
         self.conditions = conditions
         self.substitute = None
         for c in self.conditions:
-            if c.stop_return is not None:
+            if c.stop_return() is not None:
                 raise TypeError("\"conditions\" argument cannot contain Conditions with stop_return, only the"
                                 "\"completed\" argument can have it.")
         self.completed = completed
-        if self.completed.stop_return is None:
+        if self.completed.stop_return() is None:
             raise TypeError("\"completed\" argument must have the stop_return attribute set")
         self.result = None
         self.done = False
@@ -65,16 +78,21 @@ class Fork:
             return
         if self.quick_stop and self.completed.meet(u_.content):
             self.completed.callback(u_.content)
+            self.result = self.completed.stop_return(u_.content)
             self.done = True
             return
+        meet = False
         for c in self.conditions:
             if c.meet(u_.content):
+                meet = True
                 c.callback(u_.content)
                 if self.exclusive:
                     break
         if self.completed.meet(u_.content):
+            self.completed.callback(u_.content)
+            self.result = self.completed.stop_return(u_.content)
             self.done = True
-        else:
+        elif meet:
             print(":: warning: fork has matched, but is still running")
 
     def join(self):
@@ -84,21 +102,29 @@ class Fork:
                 break
             sleep(.2)
 
+    def get_result(self):
+        return self.result
+
 
 class Forks:
     def __init__(self):
         self.forks = {}
 
     def attach(self, *conds: Condition, completed: Condition, exclusive=False, custom_id=None):
-        u_ = uuid4() if custom_id is None else custom_id
-        self.forks[u_] = Fork(*conds, completed=completed, exclusive=exclusive)
+        return self.attach_fork(Fork(*conds, completed=completed, exclusive=exclusive), custom_id)
+
+    def attach_fork(self, fork: Fork, custom_id=None):
+        if not isinstance(fork, Fork):
+            raise TypeError(fork)
+        u_ = str(uuid4()) if custom_id is None else str(custom_id)
+        self.forks[u_] = fork
         return u_
 
     def detach(self, id_):
         self.forks.pop(id_)
 
-    def replace(self, id_, *conds: Condition, completed: Condition, exclusive=False, quick_stop=False):
-        new_fork = Fork(*conds, completed=completed, exclusive=exclusive, quick_stop=quick_stop)
+    def replace(self, id_, *conds: Condition, completed: Condition, exclusive=False):
+        new_fork = Fork(*conds, completed=completed, exclusive=exclusive)
         self.get(id_).substitute = new_fork
         self.forks[id_] = new_fork
 
@@ -108,6 +134,16 @@ class Forks:
 
     def get(self, id_) -> Fork:
         return self.forks[id_]
+
+    def attach_join_detach(self, *args, **kwargs):
+        if len(args) == 1 and isinstance(args[0], Fork):
+            u = self.attach_fork(*args, **kwargs)
+        else:
+            u = self.attach(*args, **kwargs)
+        self.get(u).join()
+        ret = self.get(u).result
+        self.detach(u)
+        return ret
 
 
 def wait_for(t: TelegramBot,
@@ -119,7 +155,6 @@ def wait_for(t: TelegramBot,
 
     if timeout == 0:
         infinite = True
-        timeout_end = datetime.now()
     else:
         infinite = False
         timeout_end = datetime.now() + timedelta(seconds=timeout)
@@ -129,14 +164,24 @@ def wait_for(t: TelegramBot,
             for c in conditions:
                 if c.meet(u.content):
                     c.callback(u.content)
-                    if c.stop_return is not None:
-                        if isinstance(c.stop_return, Callable):
-                            return c.stop_return(u.content)
-                        else:
-                            return c.stop_return
+                    if c.stop_return() is not None:
+                        return c.stop_return(u.content)
                     continue
             if forks:
                 forks.send(u)
-        if not infinite and timeout_end < datetime.now():
-            return False
+        if not infinite:
+            if timeout_end < datetime.now():
+                return False
         sleep(0.1)
+
+
+def wait_for_threaded(t: TelegramBot,
+                      *conditions: Condition,
+                      timeout=0,
+                      forks=None):
+    th = Thread(target=wait_for, args=(t, ) + conditions, kwargs={"timeout": timeout, "forks": forks},
+                daemon=True)
+    th.start()
+    return th
+
+
